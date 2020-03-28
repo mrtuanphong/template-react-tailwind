4a8354c76b1f17f04cb78bbb0c1c6d9fc9a306b7
Phong Do <mrtuanphong@gmail.com>
2020-03-28 16:51:11 +0700

    Updates

---
+++ precache-manifest.4a7a526c29ccc27da9622c2dc45f2bc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4bfdd1f86b0c1bc6cb091b59a46987",
+    "url": "/react-tailwind/index.html"
+  },
+  {
+    "revision": "40dbf51159b9382fe8d9",
+    "url": "/react-tailwind/static/css/main.4a9ff4a6.chunk.css"
+  },
+  {
+    "revision": "70b118c1d4a8d092a926",
+    "url": "/react-tailwind/static/js/2.5e584585.chunk.js"
+  },
+  {
+    "revision": "40dbf51159b9382fe8d9",
+    "url": "/react-tailwind/static/js/main.5d92ac2e.chunk.js"
+  },
+  {
+    "revision": "26e0c4deec86a952e77f",
+    "url": "/react-tailwind/static/js/runtime-main.cdbbba73.js"
+  },
+  {
+    "revision": "15dcea7712bc38ec90426b4a044152d1",
+    "url": "/react-tailwind/static/media/logo.15dcea77.svg"
+  }
+]);
